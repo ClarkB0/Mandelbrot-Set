b9094a45255c77c86cbba02e6df7886c2dd32ae0
ClarkB0 <clarkb@pedarecc.sa.edu.au>
2026-09-23 17:05:50 +0930

    Created function that tests C value behaviour (needs to be tested)

--- mandelbrot.py
+++ mandelbrot.py
@@ -14,6 +14,8 @@ range_imaginary = [-1.5, 1.5]
 resolution = 100
 accuracy_sig_figs = 6
 
+iterations = 10
+
 domain_size = abs(domain_real[1] - domain_real[0])
 range_size = abs(range_imaginary[1] - range_imaginary[0])
 
@@ -22,3 +24,19 @@ increment = range_size / resolution
 real_values = [round_sig_figs(domain_real[0] + increment * n, accuracy_sig_figs) for n in range(round(domain_size / increment) + 1)]
 imaginary_values = [round_sig_figs(range_imaginary[0] + increment * n, accuracy_sig_figs) for n in range(round(range_size / increment) + 1)]
 
+def rule(z, c):
+    return z ** 2 + c
+
+for real_value in real_values:
+    for imaginary_value in imaginary_values:
+        c = complex(real_value, imaginary_value)
+        z = 0
+        behaviour = None
+        for i in range(iterations):
+            z = rule(z, c)
+            if z.real ** 2 + z.imag ** 2 > 4:
+                behaviour = False
+                break
+        if behaviour == None:
+            behaviour = True
+        print(behaviour)
